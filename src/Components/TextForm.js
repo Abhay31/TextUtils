6fd9b5a995f2b57092a200044c5c37dbf2a5812e
Abhay31 <abhaysharma0983@gmail.com>
2023-02-19 03:13:07 +0530

    Added grammarly feature

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { GrammarlyEditorPlugin } from '@grammarly/editor-sdk-react'
 
 export default function TextForm(props) {
   const[text, setText] = useState('');
@@ -45,16 +46,18 @@ export default function TextForm(props) {
   return (
     <>
       <div className='container' style={{color: props.mode=== 'dark'?'white':'#042743'}}>
-          <h1>{props.heading}</h1>
-          <div className="mb-3">
-          <textarea className="form-control" value={text} placeholder="Enter Your Text Here" onChange={handleOnChange} style={{backgroundColor: props.mode=== 'light'?'white':'grey', color: props.mode=== 'dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
-          </div>
-          <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>     
-          <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
-          <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleStartClick}>Convert to StartCase</button>
-          <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-          <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleCopy}>Copy Text</button>
-          <button disabled = {text.length===0} className="btn btn-danger mx-1 my-1" onClick={handleClearFunc}>Clear Text</button>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <GrammarlyEditorPlugin clientId="client_PL9HoLjJmYkZvzFB4H3Ape">
+            <textarea className="form-control" value={text} placeholder="Enter Your Text Here" onChange={handleOnChange} style={{backgroundColor: props.mode=== 'light'?'white':'grey', color: props.mode=== 'dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+          </GrammarlyEditorPlugin>
+        </div>
+        <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>     
+        <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleStartClick}>Convert to StartCase</button>
+        <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button disabled = {text.length===0} className="btn btn-success mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+        <button disabled = {text.length===0} className="btn btn-danger mx-1 my-1" onClick={handleClearFunc}>Clear Text</button>
       </div>
       <div className="container my-3" style={{color: props.mode=== 'dark'?'white':'#042743'}}>
         <h2>Your Text Summary</h2>
